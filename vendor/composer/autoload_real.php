88c6c6d2568930b68d716c68bac23aacf900777a
Nayoki1998 <ronaldokiroyan1998@gmail.com>
2023-06-08 19:11:52 +0800

    yayay

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit98673a26f88ca785418b11004cdcd547
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit98673a26f88ca785418b11004cdcd547::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
